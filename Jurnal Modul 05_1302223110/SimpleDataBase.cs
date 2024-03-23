999f4a4419de54f6f1dd941be282c15fb22f741e
1shtalica <adibfaizulhaq05@gmail.com>
2024-03-23 08:41:10 +0700

    menambah class SimpleDataBase

---
+++ Jurnal Modul 05_1302223110/SimpleDataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_Modul_05_1302223110
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storeData;
+
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase() 
+        {
+            storeData = new List<T>();
+            inputDates = new List<DateTime>();  
+
+        }
+
+        public void AddNewData(T data)
+        {
+            storeData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storeData.Count; i++)
+            {
+                Console.WriteLine($"Data {i+1} berisi: {storeData.ElementAt(i)}, yang disimpan pada waktu UTC: {inputDates.ElementAt(i)}");
+            }
+        }
+    }
+}
